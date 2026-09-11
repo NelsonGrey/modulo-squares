c82db02adbbb4b7450610d0e40f7e621b4ecdb47
Mark Nelson <nelson.mark.a@gmail.com>
2026-09-11 09:43:32 -0500

    chore: untrack build artifacts and generated screenshots

--- packages/firebase-utils/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { FirebaseClient } from './client.js';
-export type { FirebaseConfig } from './client.js';
-export { AuthHelpers, FirestoreCrudHelpers, FunctionsHelpers, FunctionsAuthHelpers, StorageHelpers } from './client.js';
